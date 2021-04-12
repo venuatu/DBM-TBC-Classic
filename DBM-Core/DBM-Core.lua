7afe401e1a18adde449b274173a53e9a5495919a
MysticalOS <mysticalosx@gmail.com>
2021-04-12 18:51:47 -0400

    Use WOW_PROJECT_BURNING_CRUSADE_CLASSIC now that it exists

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -1383,8 +1383,7 @@ do
 			end
 			onLoadCallbacks = nil
 			loadOptions(self)
-			--if WOW_PROJECT_ID ~= WOW_PROJECT_CLASSIC then--TODO, fix this for TBC
-			if wowTOC ~= 20501 then--Temporary
+			if WOW_PROJECT_ID ~= WOW_PROJECT_BURNING_CRUSADE_CLASSIC then
 				self:Disable(true)
 				self:Schedule(15, infniteLoopNotice, self, L.CLASSIC_ONLY)
 				return
@@ -3918,7 +3917,7 @@ do
 		if not self.Options.SpecialWarningFont or (self.Options.SpecialWarningFont == "Fonts\\2002.TTF" or self.Options.SpecialWarningFont == "Fonts\\ARKai_T.ttf" or self.Options.SpecialWarningFont == "Fonts\\blei00d.TTF" or self.Options.SpecialWarningFont == "Fonts\\FRIZQT___CYR.TTF" or self.Options.SpecialWarningFont == "Fonts\\FRIZQT__.TTF") then
 			self.Options.SpecialWarningFont = "standardFont"
 		end
-		if WOW_PROJECT_ID == WOW_PROJECT_MAINLINE then return end--Don't do sound migration in a situation user is loading wrong DBM version, to avoid sound path corruption
+		if WOW_PROJECT_ID ~= WOW_PROJECT_BURNING_CRUSADE_CLASSIC then return end--Don't do sound migration in a situation user is loading wrong DBM version, to avoid sound path corruption
 		--Migrate user sound options to soundkit Ids if selected media doesn't exist in Interface\\AddOns
 		local migrated = false
 		if type(self.Options.RaidWarningSound) == "string" and self.Options.RaidWarningSound ~= "" then
