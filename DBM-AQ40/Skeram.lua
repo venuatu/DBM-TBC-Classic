be2d9f5db0e1c9cd53da26cb039fc18af2a23700
MysticalOS <mysticalosx@gmail.com>
2021-05-22 20:50:59 -0400

    Fix Skeram kill detection to ignore CID going forward do to changes in BCC

--- DBM-AQ40/Skeram.lua
+++ DBM-AQ40/Skeram.lua
@@ -6,6 +6,9 @@ mod:SetCreatureID(15263)
 mod:SetEncounterID(709)
 mod:SetModelID(15345)
 mod:SetUsedIcons(4, 5, 6, 7, 8)
+mod.noBossDeathKill = true
+mod:SetHotfixNoticeRev(20200522000000)--2021-05-22
+mod:SetMinSyncRevision(20200522000000)
 
 mod:RegisterCombat("combat")
 
