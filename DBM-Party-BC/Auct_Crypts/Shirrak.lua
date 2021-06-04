f6054cfa3fed8725dd40287c90cf4d747cb52d75
QartemisT <QartemisT@gmail.com>
2021-06-04 02:56:14 +0100

    Add warning on Focus Fire to other members.

--- DBM-Party-BC/Auct_Crypts/Shirrak.lua
+++ DBM-Party-BC/Auct_Crypts/Shirrak.lua
@@ -11,6 +11,8 @@ mod:RegisterEventsInCombat(
 	"CHAT_MSG_RAID_BOSS_EMOTE"
 )
 
+local warnFocusFire		= mod:NewTargetAnnounce(32300, 3)
+
 local specWarnFocusFire	= mod:NewSpecialWarningDodge(32300, nil, nil, nil, 1, 2)
 
 function mod:CHAT_MSG_RAID_BOSS_EMOTE(_, _, _, _, target)
@@ -18,5 +20,7 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(_, _, _, _, target)
 	if targetname == UnitName("player") then
 		specWarnFocusFire:Show()
 		specWarnFocusFire:Play("watchstep")
+	else
+		warnFocusFire:Show(target)
 	end
 end
