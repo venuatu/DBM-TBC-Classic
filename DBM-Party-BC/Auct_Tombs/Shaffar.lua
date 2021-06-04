07ef6d4d9a1141df6e05fd086628ae097c41a351
QartemisT <QartemisT@gmail.com>
2021-06-04 17:23:03 +0100

    Mana Tombs done;
    Last 2 drycodes didn't really have many mechanics, so added what I could see.

--- DBM-Party-BC/Auct_Tombs/Shaffar.lua
+++ DBM-Party-BC/Auct_Tombs/Shaffar.lua
@@ -1,7 +1,6 @@
 local mod	= DBM:NewMod(537, "DBM-Party-BC", 8, 250)
 local L		= mod:GetLocalizedStrings()
 
-
 mod:SetRevision("@file-date-integer@")
 mod:SetCreatureID(18344)
 mod:SetEncounterID(1899)
@@ -9,4 +8,20 @@ mod:SetEncounterID(1899)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
+	"UNIT_SPELLCAST_SUCCEEDED"
 )
+
+local specWarnAdds	= mod:NewSpecialWarningAdds(32371, "-Healer", nil, nil, 1, 2)
+
+function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, spellId)
+	if spellId == 32371 then
+		self:SendSync("Adds")
+	end
+end
+
+function mod:OnSync(msg)
+	if msg == "Adds" then
+		specWarnAdds:Show()
+		specWarnAdds:Play("killmob")
+	end
+end
--- DBM-Party-BC/Auct_Tombs/Yor.lua
+++ DBM-Party-BC/Auct_Tombs/Yor.lua
@@ -11,4 +11,13 @@ mod:SetEncounterID(250)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
+	"SPELL_CAST_SUCCESS"
 )
+
+local warnStomp	= mod:NewSpellAnnounce(36405, 2)
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args.spellId == 36405 then
+		warnStomp:Show()
+	end
+end
