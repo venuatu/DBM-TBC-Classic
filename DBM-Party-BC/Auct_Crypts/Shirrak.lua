5c7b4bae4fb9e4ed9d19a52fddb9b484cb52fb97
QartemisT <QartemisT@gmail.com>
2021-06-04 02:00:20 +0100

    Auct_Crypts warnings.

--- DBM-Party-BC/Auct_Crypts/Shirrak.lua
+++ DBM-Party-BC/Auct_Crypts/Shirrak.lua
@@ -8,4 +8,14 @@ mod:SetEncounterID(1890)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
+	"CHAT_MSG_RAID_BOSS_EMOTE"
 )
+
+local specWarnFocusFire	= mod:NewSpecialWarningMove(11528, nil, nil, nil, 1, 2)
+
+function mod:CHAT_MSG_RAID_BOSS_EMOTE(_, _, _, _, target, _, _, _, spellId)
+	local targetname = DBM:GetUnitFullName(target) or target
+	if spellId == 11528 and targetname == UnitName("player") then
+		specWarnFocusFire:Show()
+	end
+end
