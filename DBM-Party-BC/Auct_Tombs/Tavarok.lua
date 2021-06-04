239e9bf2ef77fd451095164383c6c43564dfc0b4
QartemisT <QartemisT@gmail.com>
2021-06-04 15:41:01 +0100

    minsync

--- DBM-Party-BC/Auct_Tombs/Tavarok.lua
+++ DBM-Party-BC/Auct_Tombs/Tavarok.lua
@@ -6,6 +6,7 @@ mod:SetRevision("@file-date-integer@")
 mod:SetCreatureID(18343)
 mod:SetEncounterID(1901)
 mod:DisableEEKillDetection() -- EE instantly fires
+mod:SetMinSyncRevision(20210604000000)--2021, 06, 04
 
 mod:RegisterCombat("combat")
 
